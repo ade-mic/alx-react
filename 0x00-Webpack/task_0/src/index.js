0415761be737d91215a2f88a99e9fec3c947b842
ade-mic <ademic.aina@gmail.com>
2024-06-26 13:21:04 +0100

    task_0

--- 0x00-Webpack/task_0/src/index.js
+++
@@ -1,8 +0,0 @@
-import $ from 'jquery';
-
-$(function() {
-    $(body).add('<p>Holberton Dashboard</p>');
-    $(body).add('<p>Dashboard data for the students</p>');
-    $(body).add('<p>Copyright - Holberton School</p>');
-})
-
