d9235219167f80f17547087782a9cdfa8b216db2
ade-mic <ademic.aina@gmail.com>
2024-06-26 16:09:51 +0100

    add

--- 0x00-Webpack/task_1/webpack.config.js
+++ 0x00-Webpack/task_1/webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path');
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
-    filename: '[name].bundle.js',
+    filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
   },
   mode: 'production',
