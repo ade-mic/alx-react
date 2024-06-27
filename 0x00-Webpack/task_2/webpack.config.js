7039118b29023c7344ed38d965043a54a038822c
ade-mic <ademic.aina@gmail.com>
2024-06-27 12:24:16 +0100

    chek

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,11 +1,12 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'public'),
-    clean: true
+    clean: true,
   },
   mode: 'production',
   module: {
@@ -14,45 +15,23 @@ module.exports = {
         test: /\.css$/i, 
         use: ["style-loader", "css-loader"] 
       },
-      {
-        test: /\.(ico|gif|png|jpg|jpeg)$/i,
+      { 
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // Skip optimization in development mode
-              disable: true, // Disable the loader (useful for development)
-              mozjpeg: {
-                progressive: true,
-                quality: 75,
-              },
-              optipng: {
-                enabled: true,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75,
-              },
-            },
-          },
-        ],
+        generator: {
+          filename: 'assets/[name].[ext]',
+        }
       },
     ]
   },
   plugins: [
-    new HtmlWebpackPlugin()
+    new HtmlWebpackPlugin({
+      template: './index.html'
+    }),
   ],
   performance: {
     hints: false,
     maxAssetSize: 1512000,
     maxEntrypointSize: 1512000,
   }
-
 };
