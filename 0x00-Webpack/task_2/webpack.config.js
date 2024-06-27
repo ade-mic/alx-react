c7411bf52b6b744457fd52c793197b3bb3f3d8d5
ade-mic <ademic.aina@gmail.com>
2024-06-27 12:53:29 +0100

    webpack.config.js

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -1,65 +1,36 @@
+// webpack.config.js
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: './js/dashboard_main.js',
   output: {
     filename: 'bundle.js',
-    path: path.resolve(__dirname, 'task_2/public'),
-    clean: true, // Clean the output directory before emit.
+    path: path.resolve(__dirname, 'public'),
+    clean: true,
   },
   mode: 'production',
   module: {
     rules: [
-      {
-        test: /\.css$/i,
-        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      { 
+        test: /\.css$/i, 
+        use: ["style-loader", "css-loader"] 
       },
-      {
-        test: /\.(ico|gif|png|jpg|jpeg)$/i,
+      { 
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
         type: 'asset/resource',
-        use: [
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              bypassOnDebug: true, // Skip optimization in development mode
-              disable: true, // Disable the loader (useful for development)
-              mozjpeg: {
-                progressive: true,
-                quality: 75,
-              },
-              optipng: {
-                enabled: true,
-              },
-              pngquant: {
-                quality: [0.65, 0.90],
-                speed: 4,
-              },
-              gifsicle: {
-                interlaced: false,
-              },
-              webp: {
-                quality: 75,
-              },
-            },
-          },
-        ],
+        generator: {
+          filename: 'assets/[name].[ext]',
+        }
       },
-    ],
+    ]
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: '[name].css',
-    }),
+    new HtmlWebpackPlugin(),
   ],
-  optimization: {
-    minimizer: [
-      `...`,
-      new CssMinimizerPlugin(),
-    ],
-  },
   performance: {
-    maxAssetSize: 512000, // Increase asset size limit to 500 KiB
-  },
+    hints: false,
+    maxAssetSize: 1512000,
+    maxEntrypointSize: 1512000,
+  }
 };
