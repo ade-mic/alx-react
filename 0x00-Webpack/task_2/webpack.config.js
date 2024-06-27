eb030700829ed4171bca73708ca4b9af7fe06531
ade-mic <ademic.aina@gmail.com>
2024-06-27 10:40:37 +0100

    2-task

---
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+module.exports = {
+  entry: './js/dashboard_main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public'),
+    clean: true
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      { 
+        test: /\.css$/i, 
+        use: ["style-loader", "css-loader"] 
+      },
+      { 
+        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'assets/[name].[ext]',
+        }
+      },
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  performance: {
+    hints: false,
+    maxAssetSize: 1512000,
+    maxEntrypointSize: 1512000,
+  }
+
+};
