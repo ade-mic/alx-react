7f126997343dc1daa0422105181ddab5b9e74c29
ade-mic <ademic.aina@gmail.com>
2024-10-31 08:15:24 +0100

    alx-react

---
+++ 0x00-Webpack/task_5/dashboard/config/webpack.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development', // Set the mode to 'development' or 'production'
+  module: {
+    rules: [
+      {
+        test: /\.js$|jsx/, // Match both .js and .jsx files
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'], // Automatically resolve certain extensions
+  },
+};
